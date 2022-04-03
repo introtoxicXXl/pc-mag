57d360fed1729c2edab11bf053eab2158abadced
introtoxicXXl <minhajulabedin648@gmail.com>
2022-04-04 03:24:51 +0600

    home page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,10 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='home-container'>
-      <div className="left-side">
-        <div className='home-info'>
-          <h1>Supercharged for pros.</h1>
-          <p>The most powerful MacBook Pro ever is here. With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life. Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast</p>
-        </div>
-      </div>
-      <div className="right-side">
+      <div className="home-info">
+        <h1>Supercharged for pros.</h1>
         <img src={picture} alt="" />
+        <p>The most powerful MacBook Pro ever is here. With the blazing-fast M1 Pro or M1 Max chip the first Apple silicon designed for pros you get groundbreaking performance and amazing battery life. Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast</p>
       </div>
     </div>
   );
