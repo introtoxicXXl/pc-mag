c2131c346ce775580d29918b556b63c77120ce78
introtoxicXXl <minhajulabedin648@gmail.com>
2022-04-04 19:00:38 +0600

    active route added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const navLinkStyle=({isActive})=>{
+    return{
+      fontWeight : isActive ? 'bold':'normal',
+      color :isActive ? 'rgb(192, 80, 52)':'black'
+    }
+  }
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/review">Review</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/blogs">Blogs</Link>
-      <Link to="/notfound"></Link>
+      <NavLink style={navLinkStyle} to="/">Home</NavLink>
+      <NavLink style={navLinkStyle} to="/review">Review</NavLink>
+      <NavLink style={navLinkStyle} to="/dashboard">Dashboard</NavLink>
+      <NavLink style={navLinkStyle} to="/blogs">Blogs</NavLink>
+      <NavLink style={navLinkStyle} to="/notfound"></NavLink>
     </nav>
   );
 };
